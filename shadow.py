66ecf5ad7a0e51e3d104df1895cd4a6cd3c5ba00
iiPython <ben@iipython.dev>
2025-11-14 23:43:43 -0600

    dont bother decoding stuff and attempt to reduce overhead

--- shadow.py
+++ shadow.py
@@ -6,26 +6,28 @@ import typing
 import asyncio
 from dataclasses import dataclass
 
+__version__ = "shdw/1.0.0"
+
 # Initialization
-UNRESERVED  = r"A-Za-z0-9\-._~"
-SUB_DELIMS  = r"!$&'()*+,;="
-PCHAR       = rf"{UNRESERVED}{SUB_DELIMS}:@%"
-QUERY_CHARS = rf"{PCHAR}/?"
+UNRESERVED  = b"A-Za-z0-9\\-._~"
+SUB_DELIMS  = b"!$&'()*+,;="
+PCHAR       = UNRESERVED + SUB_DELIMS + b":@%"
+QUERY_CHARS = PCHAR + b"/?"
 
-REQUEST_TARGET = rf"/[{PCHAR}/]*(?:\?[{QUERY_CHARS}]*)?"
-TOKEN          = r"[!#$%&'*+\-\.^_`|~0-9A-Za-z]+"
-VERSION        = r"(\d+(?:\.\d+)?)"
-FIELD_VALUE    = r"[\x20-\x7E]*"
+REQUEST_TARGET = b"/[" + PCHAR + b"/]*(?:\\?[" + QUERY_CHARS + b"]*)?"
+TOKEN       = b"[!#$%&'*+\\-\\.^_`|~0-9A-Za-z]+"
+VERSION     = b"(\\d+(?:\\.\\d+)?)"
+FIELD_VALUE = b"[\\x20-\\x7E]*"
 
-HTTP_REQUEST_LINE = re.compile(rf"^({TOKEN})\s({REQUEST_TARGET})\sHTTP/{VERSION}$")
-HTTP_HEADER_LINE  = re.compile(rf"^({TOKEN}):\s*({FIELD_VALUE})\s*$")
+HTTP_REQUEST_LINE = re.compile(b"^(" + TOKEN + b") (" + REQUEST_TARGET + b") HTTP/" + VERSION + b"$")
+HTTP_HEADER_LINE = re.compile(b"^(" + TOKEN + b"):\\s*(" + FIELD_VALUE + b")\\s*$")
 
 # Intermediaries
 @dataclass
 class Declaration:
-    method:  str | None
-    uri:     str | None
-    version: str | None
+    method:  bytes | None
+    uri:     bytes | None
+    version: bytes | None
 
 @dataclass
 class Response:
@@ -42,15 +44,15 @@ class HTTPException(Exception):
 class Request:
     def __init__(self, source: tuple[str, int]) -> None:
         self.declaration: Declaration = Declaration(None, None, None)
-        self.headers: dict[str, str] = {}
+        self.headers: dict[bytes, bytes] = {}
         self.source: tuple[str, int] = source
 
         self._body: bytes = b""
 
     def consume(self, line: bytes) -> None:
-        processed_line = line.decode("utf-8").removesuffix("\r\n")
+        processed_line = line[:-2]
         if self.declaration.method is None:
-            if processed_line == "PRI * HTTP/2.0":
+            if processed_line == b"PRI * HTTP/2.0":
                 raise HTTPException(505, "Shadow does not support HTTP/2.")
 
             declaration = HTTP_REQUEST_LINE.match(processed_line)
@@ -92,7 +94,7 @@ class Shadow:
                 for name, value in (response.headers | {
                     "connection": "close",
                     "content-length": str(len(response.body)),
-                    "server": "shadow"
+                    "server": __version__
                 }).items()
             ],
             b"\r\n" + response.body
@@ -100,33 +102,32 @@ class Shadow:
 
     async def handle_connection(self, read_stream: asyncio.StreamReader, write_stream: asyncio.StreamWriter) -> None:
         request, response = Request(write_stream.get_extra_info("peername")[:2]), None
+        try:
 
-        # Feed data into request from client
-        async for item in read_stream:
-            if item == b"\r\n":
-                break
+            # Feed data into request from client
+            async for item in read_stream:
+                if item == b"\r\n":
+                    break
 
-            try:
                 request.consume(item)
 
-            except HTTPException as k:
-                response = self.error(k.status_code, k.message)
-                break
+            # Check for data
+            content_length = request.headers.get(b"content-length")
+            if content_length is not None:
+                if not content_length.decode("utf-8").isnumeric():
+                    raise HTTPException(400, "Invalid content length provided.")
 
-        # Check for data
-        content_length = request.headers.get("content-length")
-        if content_length is not None:
-            if not content_length.isnumeric():
-                response = self.error(400, "Invalid content length provided.")
-
-            else:
                 request._set_body(await read_stream.read(int(content_length)))
 
-        # Fetch response
-        response = response or await self.on_request(request)
-        if response is not None:
-            write_stream.write(self.dump_response(response))
-            await write_stream.drain()
+            # Fetch response
+            response = await self.on_request(request)
+            if response is not None:
+                write_stream.write(self.dump_response(response))
+
+        except HTTPException as k:
+            write_stream.write(self.dump_response(self.error(k.status_code, k.message)))
+
+        await write_stream.drain()
 
         # Clean up
         write_stream.close()
